64f6cfc54b0722364e3ea61518c0bf67334f71a1
h0angpc <phanchauhoang2004@gmail.com>
2023-11-20 22:18:27 +0700

    xoa comment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,9 +5,6 @@ using System.Windows.Media;
 
 namespace SpaManagement
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
