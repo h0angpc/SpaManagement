ddec14da4dbdc949064ff16b51201238b309d87d
Bao <22520111@gm.uit.edu.vn>
2023-12-16 22:47:43 +0700

    update PaymentView

--- Views/EditCustomerView.xaml.cs
+++ Views/EditCustomerView.xaml.cs
@@ -31,5 +31,10 @@ namespace SpaManagement.Views
                 this.DragMove();
             }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
