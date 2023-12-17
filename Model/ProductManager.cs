cf3a177b46b6500d24bbfb484f7584d33fa7ce07
AnhNguyen <22520060@gm.uit.edu.vn>
2023-12-17 23:39:31 +0700

    EditProductView, Basic ProductViewModel

---
+++ Model/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+
+namespace SpaManagement.Model
+{
+    public class ProductManager
+    {
+        public static ObservableCollection<PRODUCT> _DatabaseCustomers = new ObservableCollection<PRODUCT>(DataProvider.Ins.DB.PRODUCTs);
+
+        public static ObservableCollection<PRODUCT> GetProducts()
+        {
+            return _DatabaseCustomers; 
+        }
+
+        public static void AddProduct(PRODUCT product)
+        {
+            _DatabaseCustomers.Add(product);   
+        }
+    }
+}
