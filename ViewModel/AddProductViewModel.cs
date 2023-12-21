f8bbf6676227a4ac87c19a2aa04ab0bf680e292b
AnhNguyen <22520060@gm.uit.edu.vn>
2023-12-21 17:24:12 +0700

    95% Product Page

--- ViewModel/AddProductViewModel.cs
+++ ViewModel/AddProductViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SpaManagement.Model;
+﻿using Microsoft.Win32;
+using SpaManagement.Model;
 using SpaManagement.Views;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.Xml.Linq;
@@ -26,17 +28,18 @@ namespace SpaManagement.ViewModel
         private string _ProductLink;
         public string ProductLink { get => _ProductLink; set { _ProductLink = value; OnPropertyChanged(); } }
 
-
         private BitmapImage _ProductImage;
         public BitmapImage ProductImage { get => _ProductImage; set { _ProductImage = value; OnPropertyChanged(); } }
-        public ICommand AddProductCommand { get; set; }   
+        public ICommand AddProductCommand { get; set; }
+        public ICommand ImageProduct { get; set; }
 
+        public ICommand CloseCommand { get; set; }
         public AddProductViewModel()
         {
 
             AddProductCommand = new RelayCommand<object>((p) =>
             {
-                if (string.IsNullOrEmpty(ProductName))
+                if (string.IsNullOrEmpty(ProductName) || string.IsNullOrEmpty(ProductLink) || string.IsNullOrEmpty(ProductPrice.ToString()) || ProductImage == null)
                 {
                     return false;
                 }
@@ -48,7 +51,7 @@ namespace SpaManagement.ViewModel
                 return true;
             }, (p) =>
             {
-                var Product = new PRODUCT() { PRO_NAME = ProductName, PRICE = ProductPrice };
+                var Product = new PRODUCT() { PRO_NAME = ProductName, PRICE = ProductPrice, PRO_URL = ProductLink, PRO_IMG = ProductImage.ToString() };
 
                 DataProvider.Ins.DB.PRODUCTs.Add(Product);
                 DataProvider.Ins.DB.SaveChanges();
@@ -58,7 +61,39 @@ namespace SpaManagement.ViewModel
                 MessageBoxCustom m = new MessageBoxCustom("Thêm sản phẩm mới thành công", MessageType.Info, MessageButtons.Ok);
                 m.ShowDialog();
             });
+            ImageProduct = new RelayCommand<object>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.tif;...";
+                openFileDialog.InitialDirectory = @"C:\";
+                openFileDialog.Title = "Vui lòng chọn ảnh.";
+                if(openFileDialog.ShowDialog() == true)
+                {
+                    Uri fileUri = new Uri(openFileDialog.FileName);
+                    ProductImage = new BitmapImage(fileUri);
+                    string sourcefile = openFileDialog.FileName;
+                    string resourceUri = "..//..//Image//" + System.IO.Path.GetFileName(openFileDialog.FileName);
+                    var list1 = DataProvider.Ins.DB.PRODUCTs.Where(x => x.PRO_IMG == resourceUri);
+                    if (list1 != null)
+                    {
 
+                    }
+                    else
+                    {
+                        System.IO.File.Copy(sourcefile, resourceUri, true);// Nếu chưa có thì lưu ảnh
+                    }
+                }
+            });
+            CloseCommand = new RelayCommand<Window>((p) => { return p == null ? false : true; }, (p) => {
+                var w = (p);
+                if (w != null)
+                {
+                    w.Close();
+                }
+            });
         }
 
     }
--- ViewModel/ProductViewModel.cs
+++ ViewModel/ProductViewModel.cs
@@ -16,44 +16,126 @@ namespace SpaManagement.ViewModel
 {
     public class ProductViewModel : BaseViewModel
     {
+
+        private string _ProductName;
+        public string ProductName { get => _ProductName; set { _ProductName = value; OnPropertyChanged(); } }
+
+        private decimal _ProductPrice;
+        public decimal ProductPrice { get => _ProductPrice; set { _ProductPrice = value; OnPropertyChanged(); } }
+
+        private string _ProductLink;
+        public string ProductLink { get => _ProductLink; set { _ProductLink = value; OnPropertyChanged(); } }
+
+        private BitmapImage _ProductImage;
+        public BitmapImage ProductImage { get => _ProductImage; set { _ProductImage = value; OnPropertyChanged(); } }
+
         private ObservableCollection<PRODUCT> _ProductList;
         public ObservableCollection<PRODUCT> ProductList { get => _ProductList; set { _ProductList = value; OnPropertyChanged(); } }
         public ICommand ShowAddProCommand { get; set; }
         public ICommand ShowEditProCommand { get; set; }
+        public ICommand LinkButtonCommand { get; set; }
+
+        private string _textToFilter;
+
+        public string TextToFilter
+        {
+            get { return _textToFilter; }
+            set { _textToFilter = value;
+                OnPropertyChanged(nameof(TextToFilter));
+                ProductCollection.Filter = FilterByName;
+            }
+        }
+
+
+        private string ImagePath;
 
         private ICollectionView _productcollection;
-
         public ICollectionView ProductCollection
         {
             get { return _productcollection; }
             set {  _productcollection = value; OnPropertyChanged(); }
         }
 
-        private PRODUCT _SelectedItem;
-        public PRODUCT SelectedItem
+        private PRODUCT _SelectedProduct;
+        public PRODUCT SelectedProduct
         {
-            get => _SelectedItem;
+            get => _SelectedProduct;
             set
             {
-                _SelectedItem = value;
+                _SelectedProduct = value;
                 OnPropertyChanged();
-                if (SelectedItem != null)
-                {
-
-
-                }
             }
         }
 
 
         public ProductViewModel()
         {
-            ShowAddProCommand = new RelayCommand<object>((p) => { return true; }, (p) => { AddProductView wd = new AddProductView(); wd.ShowDialog(); });
-            //_ProductList = ProductManager.GetProducts();
-            //ProductCollection = CollectionViewSource.GetDefaultView(_ProductList); 
-            ProductList = new ObservableCollection<PRODUCT>(DataProvider.Ins.DB.PRODUCTs);
+            ShowAddProCommand = new RelayCommand<object>((p) => 
+            { 
+                return true;
+            }, (p) => 
+            { 
+                AddProductView wd = new AddProductView();
+                wd.ShowDialog();
+            });
+            _ProductList = ProductManager.GetProducts();
+            ProductCollection = CollectionViewSource.GetDefaultView(_ProductList);
+
+            LinkButtonCommand = new RelayCommand<object>((p) =>
+            {
+                if (SelectedProduct == null)
+                    return false;
+                return true;
+            },
+            (p) =>
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start(new ProcessStartInfo
+                    {
+                        FileName = SelectedProduct.PRO_URL,
+                        UseShellExecute = true
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error opening URL: " + ex.Message);
+                }
+
+
+            });
+
+            ShowEditProCommand = new RelayCommand<PRODUCT>((p) =>
+            {
+                if(SelectedProduct == null)
+                    return false;
+                return true;
+            },
+            (p) =>
+            {
+                EditProductViewModel editProductViewModel = new EditProductViewModel(p);
+                EditProductView wd = new EditProductView();
+                wd.DataContext = editProductViewModel;
+                wd.ShowDialog();
+
+            });
         }
-        
-        
+        private bool FilterByName(object pro)
+        {
+            if (!string.IsNullOrEmpty(TextToFilter))
+            {
+                var proDetail = pro as PRODUCT;
+                if (proDetail != null)
+                {
+                    string filtertext = TextToFilter.ToLower();
+                    string productName = proDetail.PRO_NAME.ToLower();
+
+                    return productName.Contains(filtertext);
+                }
+            }
+            return true;
+        }
+
+
     }
 }
