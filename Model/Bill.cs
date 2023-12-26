3f01b57f0fd90cd57873acf3730bc3d16892b9cc
Bao <22520111@gm.uit.edu.vn>
2023-12-26 19:11:25 +0700

    update PrintBillViewModel and Bill

---
+++ Model/Bill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaManagement.Model
+{
+    public class Bill
+    {
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public decimal AMOUNT { get; set; }
+    }
+}
