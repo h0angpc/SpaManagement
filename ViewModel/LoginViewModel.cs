a990ccfd80d28b2c99603750468b38d82be3a460
h0angpc <phanchauhoang2004@gmail.com>
2023-12-26 10:24:51 +0700

    xong lay MK

--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -40,6 +40,7 @@ namespace SpaManagement.ViewModel
         public ICommand LoginCommand { get; set; }
 
         public ICommand PasswordChangedCommand { get; set; }
+        public ICommand ShowQuenMK {  get; set; }
 
 
         public LoginViewModel()
@@ -53,6 +54,11 @@ namespace SpaManagement.ViewModel
             {
                 PassWord = p.Password;
             });
+            ShowQuenMK = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                QuenMKView wd = new QuenMKView();
+                wd.ShowDialog();
+            });
          }
 
         void Login(Window p)
@@ -62,7 +68,7 @@ namespace SpaManagement.ViewModel
                 return;
             }
             
-            string passEncode = MD5Hash(Base64Encode(PassWord));
+            string passEncode = Base64Encode(PassWord);
 
             var accCount = DataProvider.Ins.DB.ACCOUNTs.Where(x => x.A_USERNAME == UserName && x.A_PASSWORD == passEncode).Count();
 
@@ -87,19 +93,6 @@ namespace SpaManagement.ViewModel
         {
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             return System.Convert.ToBase64String(plainTextBytes);
-        }
-
-        public static string MD5Hash(string input)
-        {
-            StringBuilder hash = new StringBuilder();
-            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
-            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
-
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                hash.Append(bytes[i].ToString("x2"));
-            }
-            return hash.ToString();
         }
     }
 }
